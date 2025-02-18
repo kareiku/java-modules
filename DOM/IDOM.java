4f3cb41c2a6551494d4f1d5b7d82ff74bfea3ecc
kareiku <xaxvu7@gmail.com>
2025-02-18 20:09:30 +0100

    Added DOM package, with concrete case SQLiteDOM. Non-documented.

---
+++ DOM/IDOM.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IDOM {
+    void update(String query) throws SQLException;
+
+    List<List<?>> fetch(String query) throws SQLException;
+}
---
+++ DOM/SQLiteDOM.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class SQLiteDOM extends DOM {
+    private static final String JDBC_SQLITE = "jdbc:sqlite:";
+    private final String dbName;
+
+    public SQLiteDOM(String dbName) {
+        this.dbName = dbName;
+    }
+
+    @Override
+    protected Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_SQLITE + Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource(dbName)).getPath());
+    }
+}
